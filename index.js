75ff22716b37fdc7b763875becf2b7861904f546
Jean Damascene CYIFUZO <jeancyifuzodamas@gmail.com>
2022-03-18 22:47:19 -0700

    ft(setup): setup my back end
    WIP TP-21

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 const app = express();
 
+dotenv.config();
+
+mongoose
+    .connect("mongodb+srv://cyifuzo:Company1221@cluster0.awubt.mongodb.net/JeanShop?retryWrites=true&w=majority")
+    .then(()=>console.log("Db connected"))
+    .catch((err)=>console.log(err));
+
 app.listen(5000, ()=>{
     console.log("server is running");
-})
+})  
