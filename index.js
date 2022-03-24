13ed7b381ed9a78c561ca2f2c1600cbae60b340e
Jean Damascene CYIFUZO <jeancyifuzodamas@gmail.com>
2022-03-24 19:07:56 +0200

    login api

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
 
 const app = express();
 
 dotenv.config();
 
 mongoose
-    .connect("mongodb+srv://cyifuzo:Company1221@cluster0.awubt.mongodb.net/JeanShop?retryWrites=true&w=majority")
+    .connect("mongodb+srv://"+ process.env.MONGO_URL)
     .then(()=>console.log("Db connected"))
     .catch((err)=>console.log(err));
+
+app.use(express.json());
+app.use("/api/v1/user", userRoute)
+app.use("/api/v1/auth", authRoute) 
 
 app.listen(5000, ()=>{
     console.log("server is running");
---
+++ routes/auth.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+//====START::register============//
+
+router.post("/register", async (req,res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    });
+    try { 
+        const userSaved = await newUser.save();
+        res.status(201).json(userSaved)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+//====END::register============//
+
+//====START:: login============//
+
+router.post("/login", async (req,res)=>{
+    try {
+        const user = await User.findOne({ username: req.body.username  });
+        !user && res.status(401).json("wrong credentials!")
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password, 
+            process.env.PASS_SEC
+            );
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        originalPassword !== req.body.password && res.status(401).json("wrong credentials!");
+
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin
+            },
+                process.env.JWT_SEC, {expiresIn: "1h"}
+        );
+
+        const { password , ...others} = user._doc;
+
+        res.status(200).json({...others, accessToken});
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+//====END:: login============//
+
+module.exports = router
